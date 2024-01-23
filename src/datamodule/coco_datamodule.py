8a8ef7ed7a61d837552feb1c8bff13de6da6bc35
Michal Wilinski <m.wilinski11@gmail.com>
2024-01-24 00:10:09 +0100

    feat: augmentations

--- src/datamodule/coco_datamodule.py
+++ src/datamodule/coco_datamodule.py
@@ -1,4 +1,5 @@
 import pathlib
+from omegaconf import DictConfig
 
 import torch
 import torch.utils.data
@@ -11,33 +12,55 @@ from torch.utils.data import DataLoader
 
 
 class COCODatamodule(LightningDataModule):
-    def __init__(self, data_dir: str, batch_size: int = 32):
+    def __init__(self, data_dir: str, batch_size: int = 32, cfg: DictConfig = None):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.config = cfg
+
+    def _prepare_augmentations(self, transforms_list):
+        if self.config.transforms.SanitizeBoundingBoxes:
+            transforms_list.append(v2.SanitizeBoundingBoxes())
+        if self.config.transforms.RandomPhotometricDistort.enabled:
+            transforms_list.append(
+                v2.RandomPhotometricDistort(
+                    p=self.config.transforms.RandomPhotometricDistort.probability
+                )
+            )
+        if self.config.transforms.RandomZoomOut.enabled:
+            transforms_list.append(
+                v2.RandomZoomOut(
+                    fill=self.config.transforms.RandomZoomOut.fill,
+                )
+            )
+        if self.config.transforms.RandomIoUCrop.enabled:
+            transforms_list.append(v2.RandomIoUCrop())
+        if self.config.transforms.RandomHorizontalFlip.enabled:
+            transforms_list.append(
+                v2.RandomHorizontalFlip(
+                    p=self.config.transforms.RandomHorizontalFlip.probability
+                )
+            )
+        return transforms_list
 
     def setup(self, stage=None):
-        transforms = v2.Compose(
-            [
-                v2.ToImage(),
-                v2.RandomPhotometricDistort(p=1),
-                v2.RandomZoomOut(fill={tv_tensors.Image: (123, 117, 104), "others": 0}),
-                v2.RandomIoUCrop(),
-                v2.RandomHorizontalFlip(p=1),
-                v2.SanitizeBoundingBoxes(),
-                v2.ToDtype(torch.float32, scale=True),
-            ]
-        )
+        transforms_list = [
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+        ]
+        test_transforms = v2.Compose(transforms_list)
+        transforms_list = self._prepare_augmentations(transforms_list)
+        train_transforms = v2.Compose(transforms_list)
         self.train_dataset = datasets.CocoDetection(
             root=pathlib.Path(self.data_dir) / "train2017",
             annFile=pathlib.Path(self.data_dir)
             / "annotations/instances_train2017.json",
-            transforms=transforms,
+            transforms=train_transforms,
         )
         self.val_dataset = datasets.CocoDetection(
             root=pathlib.Path(self.data_dir) / "val2017",
             annFile=pathlib.Path(self.data_dir) / "annotations/instances_val2017.json",
-            transforms=transforms,
+            transforms=test_transforms,
         )
 
         self.train_dataset = datasets.wrap_dataset_for_transforms_v2(
